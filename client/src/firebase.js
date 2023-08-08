143e6c2ee523a95f3daa2652fea5667039890a39
Amogh <amogh18t@gmail.com>
2023-08-08 23:37:34 +0530

    updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,21 +4,12 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "FIREBASE_KEY",
-<<<<<<< HEAD
-  authDomain: "yutube-11653.firebaseapp.com",
-  projectId: "yutube-11653",
-  storageBucket: "yutube-11653.appspot.com",
-  messagingSenderId: "954689027282",
-  appId: "1:954689027282:web:142e5df58355561e57810b",
-  measurementId: "G-FXZZQ1TDDL"
-=======
   authDomain: "asuratube-6e2ed.firebaseapp.com",
   projectId: "asuratube-6e2ed",
   storageBucket: "asuratube-6e2ed.appspot.com",
   messagingSenderId: "736725398327",
   appId: "1:736725398327:web:02fa5ee1924b8a9e7760b2",
   measurementId: "G-QV824F6244"
->>>>>>> e6ea0f1 (updated)
 };
 
 const app = initializeApp(firebaseConfig);
@@ -26,8 +17,4 @@ export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 export const analytics = getAnalytics(app);
 
-<<<<<<< HEAD
 export default app;
-=======
-export default app;
->>>>>>> e6ea0f1 (updated)
